f57fbbed6b3387951df5539c51252e58d746bda5
mario <zdravkovskim1994@gmail.com>
2020-08-17 17:08:24 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SliderComponent } from './home/slider/home-slider.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    SliderComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/slider/home-slider.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-home-slider',
+    templateUrl: './home-slider.component.html',
+    styleUrls: ['./home-slider.component.css']
+
+})
+
+
+export class SliderComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+    
+    
+}
+
